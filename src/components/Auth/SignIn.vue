304efc39ab5b47492cb7a36210e776ab651559af
Anton Paraskiv <anton@paraskiv.net>
2019-11-30 22:31:17 +0300

    Added Sign In + Sign Up form validation

--- src/components/Auth/SignIn.vue
+++ src/components/Auth/SignIn.vue
@@ -1,32 +1,53 @@
 <template>
   <v-container>
 
-    <v-form>
-      <v-text-field
-        prepend-icon="mdi-email"
-        name="email"
-        label="Email"
-        type="email"
-      ></v-text-field>
-      <v-text-field
-        prepend-icon="mdi-lock"
-        name="password"
-        label="Password"
-        type="password"
-      ></v-text-field>
-    </v-form>
+      <v-form v-model="valid" ref="form">
+          <v-text-field
+              prepend-icon="mdi-email"
+              name="email"
+              label="Email"
+              type="email"
+              :rules="emailRules"
+              v-model="email"
+          ></v-text-field>
+          <v-text-field
+              prepend-icon="mdi-lock"
+              name="password"
+              label="Password"
+              type="password"
+              :counter="6"
+              :rules="passwordRules"
+              v-model="password"
+          ></v-text-field>
+      </v-form>
 
-    <v-btn
-      color="primary"
-    >Войти</v-btn>
+      <v-btn
+          color="primary"
+          :disabled="!valid"
+      >Войти</v-btn>
 
   </v-container>
 </template>
 
 <script>
+    const emailRegex = /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/
+
 export default {
-  name: 'Sign In',
-  data: () => ({
-  }),
+    data () {
+        return {
+            email: '',
+            password: '',
+            valid: false,
+            emailRules: [
+                v => !!v || 'E-mail is required',
+                v => emailRegex.test(v) || 'E-mail must be valid'
+            ],
+            passwordRules: [
+                v => !!v || 'Password is required',
+                v => (v && v.length >= 6) || 'Password must be equal or more than 6 characters'
+            ]
+        }
+    },
+
 };
 </script>
--- src/components/Auth/SignUp.vue
+++ src/components/Auth/SignUp.vue
@@ -1,38 +1,65 @@
 <template>
   <v-container>
 
-    <v-form>
+    <v-form v-model="valid" ref="form">
       <v-text-field
         prepend-icon="mdi-email"
         name="email"
         label="Email"
         type="email"
+        :rules="emailRules"
+        v-model="email"
       ></v-text-field>
       <v-text-field
         prepend-icon="mdi-lock"
         name="password"
         label="Password"
         type="password"
+        :counter="6"
+        :rules="passwordRules"
+        v-model="password"
       ></v-text-field>
       <v-text-field
         prepend-icon="mdi-lock"
         name="confirm-password"
         label="Confirm Password"
         type="password"
+        :rules="confirmPasswordRules"
+        v-model="confirmPassword"
       ></v-text-field>
     </v-form>
 
     <v-btn
       color="primary"
+      :disabled="!valid"
     >Зарегистрировать</v-btn>
 
   </v-container>
 </template>
 
 <script>
+  const emailRegex = /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/
+
 export default {
-  name: 'Sign Up',
-  data: () => ({
-  }),
+  data () {
+    return {
+      email: '',
+      password: '',
+      confirmPassword: '',
+      valid: false,
+      emailRules: [
+        v => !!v || 'E-mail is required',
+        v => emailRegex.test(v) || 'E-mail must be valid'
+      ],
+      passwordRules: [
+        v => !!v || 'Password is required',
+        v => (v && v.length >= 6) || 'Password must be equal or more than 6 characters'
+      ],
+      confirmPasswordRules: [
+        v => !!v || 'Password is required',
+        v => (v === this.password) || 'Password should match'
+      ]
+    }
+  },
 };
 </script>
