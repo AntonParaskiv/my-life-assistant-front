a2752411e1e1066bfc5a9cb3f3c7ce2f6fabacd8
Anton Paraskiv <anton@paraskiv.net>
2019-11-30 22:07:40 +0300

    Added Sign In + Sign Up forms

--- src/components/Auth/SignIn.vue
+++ src/components/Auth/SignIn.vue
@@ -1,11 +1,25 @@
 <template>
   <v-container>
-    <v-layout
-      text-center
-      wrap
-    >
-     SIGN IN
-    </v-layout>
+
+    <v-form>
+      <v-text-field
+        prepend-icon="mdi-email"
+        name="email"
+        label="Email"
+        type="email"
+      ></v-text-field>
+      <v-text-field
+        prepend-icon="mdi-lock"
+        name="password"
+        label="Password"
+        type="password"
+      ></v-text-field>
+    </v-form>
+
+    <v-btn
+      color="primary"
+    >Войти</v-btn>
+
   </v-container>
 </template>
 
--- src/components/Auth/SignUp.vue
+++ src/components/Auth/SignUp.vue
@@ -1,11 +1,31 @@
 <template>
   <v-container>
-    <v-layout
-      text-center
-      wrap
-    >
-     SIGN UP
-    </v-layout>
+
+    <v-form>
+      <v-text-field
+        prepend-icon="mdi-email"
+        name="email"
+        label="Email"
+        type="email"
+      ></v-text-field>
+      <v-text-field
+        prepend-icon="mdi-lock"
+        name="password"
+        label="Password"
+        type="password"
+      ></v-text-field>
+      <v-text-field
+        prepend-icon="mdi-lock"
+        name="confirm-password"
+        label="Confirm Password"
+        type="password"
+      ></v-text-field>
+    </v-form>
+
+    <v-btn
+      color="primary"
+    >Зарегистрировать</v-btn>
+
   </v-container>
 </template>
 
